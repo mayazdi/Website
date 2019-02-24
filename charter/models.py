ce92a70f3b411d01032c2909e10d4c1bc15c0472
Amin <yazdi.mohammad99@gmail.com>
2019-02-24 23:53:28 +0330

    Add all websites:Open file, Add functions, Add links.txt, Add links.

--- charter/models.py
+++ charter/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+import requests
+import re
 
 # Create your models here.
--- charter/views.py
+++ charter/views.py
@@ -6,6 +6,36 @@ import re
 import jdatetime
 
 
+def my_min(first, second):
+    if first == -1:
+        return second
+    elif second == -1:
+        return first
+    else:
+        return min(first, second)
+
+
+def prices():
+    with open("links.txt") as f:
+        links = f.readlines()
+    links = [x.strip('\n') for x in links]
+    result = []
+    sites = []
+    for i in range(0, 32):
+        result.append(-1)
+        sites.append('sln724.com')
+    for link in links:
+        response = requests.get('http://' + link + '/Systems/FlightCheapestPriceBar.aspx?Origin=THR&Destination=MHD')
+        res = re.findall('(\d+),000', response.text)
+        for i in range(0, 32):
+            if i < len(res):
+                temp = result[i]
+                result[i] = my_min(result[i], res[i])
+                if temp != result[i]:
+                    sites[i] = link
+    return list(zip(sites, result))
+
+
 def index(request, origin, destination):
     if origin.upper() == 'THR' and destination.upper() == 'MHD':
         response = requests.get('https://markazetour.ir/Systems/FlightCheapestPriceBar.aspx?Origin=THR&Destination=MHD')
@@ -13,13 +43,14 @@ def index(request, origin, destination):
         time = jdatetime.datetime.now()
         iter = wbc = 7 - int(time.strftime("%w"))
         weeklist = [wbc]
+        result = prices()
         month_length = (
                 jdatetime.date(int(time.year), int(time.month), 1) - jdatetime.date(int(time.year), int(time.month) - 1,
                                                                                     1)).days
         while iter + 7 <= month_length:
             iter += 7
             weeklist.append(iter)
-        if len(weeklist) == 5:
+        while len(weeklist) <= 5:
             weeklist.append(month_length)
         w1 = {}
         w2 = {}
@@ -29,22 +60,22 @@ def index(request, origin, destination):
         w6 = {}
         for i in range(0, weeklist[0]):
             if i + time.day <= month_length:
-                w1[i + time.day] = (res[i - 1], 'markaz')
+                w1[i + time.day] = result[i - 1]
         for i in range(weeklist[0], weeklist[1]):
             if i + time.day <= month_length:
-                w2[i + time.day] = res[i - 1]
+                w2[i + time.day] = result[i - 1]
         for i in range(weeklist[1], weeklist[2]):
             if i + time.day <= month_length:
-                w3[i + time.day] = res[i - 1]
+                w3[i + time.day] = result[i - 1]
         for i in range(weeklist[2], weeklist[3]):
             if i + time.day <= month_length:
-                w4[i + time.day] = res[i - 1]
+                w4[i + time.day] = result[i - 1]
         for i in range(weeklist[3], weeklist[4]):
             if i + time.day <= month_length:
-                w5[i + time.day] = res[i - 1]
+                w5[i + time.day] = result[i - 1]
         for i in range(weeklist[4], weeklist[5]):
             if i + time.day <= month_length:
-                w6[i + time.day] = res[i - 1]
+                w6[i + time.day] = result[i - 1]
         return render(request, 'Charter.html',
                       {'response': res, 'origin': origin, 'destination': destination, 'wb': time.strftime("%w"),
                        'wbc': wbc, 'w1': w1, 'w2': w2, 'w3': w3, 'w4': w4, 'w5': w5, 'w6': w6, 'mon1': 'اسفند',
